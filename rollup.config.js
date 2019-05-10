bc6b4863bb3c0d33fc5c59ce67120e3689b31202
John Sterling <john.sterling@unomaly.com>
2019-05-10 19:34:34 +0200

    Add version to bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,11 @@
 import typescript from "rollup-plugin-typescript2";
+import packagejson from "./package.json";
 
 export default {
   input: "./src/index.ts",
   plugins: [typescript(/*{ plugin options }*/)],
   output: {
+    intro: `/* fastidious ${packagejson.version} - https://github.com/johnste/fastidious */`,
     file: "dist/index.js",
     format: "umd",
     name: "fastidious"
